c304141edd1f1148d627fc5d768d0382d352be31
Frank Prößdorf <frank@naa.li>
2020-12-09 11:03:37 +0200

    Refactor Day09

--- 09/main.jl
+++ 09/main.jl
@@ -1,30 +1,24 @@
 @views function is_valid(current, preamble)
-    if length(preamble) == 1
-        false
-    else
-        n = preamble[1]
-
-        for i in preamble[2:end]
-            if n + i == current
-                return true
-            end
+    for i in preamble
+        if current - i ∈ preamble
+            return true
         end
-
-        is_valid(current, preamble[2:end])
     end
+
+    false
 end
 
 function find_invalid(preamble_size, input)
     preamble = []
 
     for i = 1:preamble_size
-        push!(preamble, popat!(input, 1))
+        push!(preamble, popfirst!(input))
     end
 
     for i in input
         if is_valid(i, preamble)
             push!(preamble, i)
-            popat!(preamble, 1)
+            popfirst!(preamble)
         else
             return i
         end
@@ -32,22 +26,26 @@ function find_invalid(preamble_size, input)
 end
 
 @views function find_contiguous(invalid, input)
-    for i = 1:length(input)
-        if sum(input[1:i]) == invalid
-            return input[1:i]
-        end
-    end
+    n = 1
 
-    find_contiguous(invalid, input[2:end])
+    while true
+        for i = n:length(input)
+            if sum(input[n:i]) == invalid
+                return input[n:i]
+            end
+        end
+
+        n += 1
+    end
 end
 
 preamble_size = 25
 input = map(l -> parse(Int, l), readlines("input.txt"))
 
 # Part 1
-invalid = find_invalid(preamble_size, copy(input))
+@time invalid = find_invalid(preamble_size, copy(input))
 println("The number that doesn't work is ", invalid)
 
 # Part 2
-numbers = find_contiguous(invalid, input)
-println("The encryption weakness is ", minimum(numbers) + maximum(numbers))
+@time numbers = find_contiguous(invalid, input)
+println("The encryption weakness is ", sum(extrema(numbers)))
